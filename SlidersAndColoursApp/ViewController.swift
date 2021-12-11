f23088af286c170168e325f6864492a546b2a886
OlDiKi <delorian101allgood@gmail.com>
2021-12-11 16:07:04 +0300

    setup mainboard and view controller ver 1.0

--- SlidersAndColoursApp/ViewController.swift
+++ SlidersAndColoursApp/ViewController.swift
@@ -23,33 +23,32 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         colorView.layer.cornerRadius = 10
         
+        redColourValue.text = String("0.30")
+        greenColourValue.text = String("0.60")
+        blueColourValue.text = String("1.0")
+        
+        redColourSlider.value = 0.30
         redColourSlider.maximumValue = 1.0
         redColourSlider.minimumValue = 0.0
         
+        greenColourSlider.value = 0.60
         greenColourSlider.maximumValue = 1.0
         greenColourSlider.minimumValue = 0.0
         
+        blueColourSlider.value = 1.0
         blueColourSlider.maximumValue = 1.0
         blueColourSlider.minimumValue = 0.0
         
     }
 
-
-    @IBAction func redSliderAction() {
+    func changeColour() {
+    colorView.backgroundColor = UIColor(red: CGFloat(redColourSlider.value), green: CGFloat(greenColourSlider.value), blue: CGFloat(blueColourSlider.value), alpha: 1)
+        
+    
+}
+    @IBAction func rgbChanged(_ sender: Any) {
+        changeColour()
         redColourValue.text = String(format: "%.2f", redColourSlider.value)
-        let redSlider = CGFloat(redColourSlider.value)
-        colorView.backgroundColor = colorView.backgroundColor?.withAlphaComponent(redSlider)
-    }
-    @IBAction func greenSliderAction() {
         greenColourValue.text = String(format: "%.2f", greenColourSlider.value)
-        let greenSlider = CGFloat(greenColourSlider.value)
-        colorView.backgroundColor = colorView.backgroundColor?.withAlphaComponent(greenSlider)
-    }
-    @IBAction func blueSliderAction() {
-        blueColourValue.text = String(format: "%.2f", blueColourSlider.value)
-        let blueSlider = CGFloat(blueColourSlider.value)
-        colorView.backgroundColor = colorView.backgroundColor?.withAlphaComponent(blueSlider)
-    }
-    
-    
+        blueColourValue.text = String(format: "%.2f", blueColourSlider.value)    }
 }
